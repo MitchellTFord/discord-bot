26c1c324c76563f489af43c20e640c96c4a0d2bc
Mitchell Ford <mitchelltford@gmail.com>
2021-12-07 13:57:09 -0600

    Add SpringBoot project with some dependencies

---
+++ src/main/java/com/mitchelltford/discordbot/DiscordbotApplication.java
@@ -0,0 +1,13 @@
+package com.mitchelltford.discordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitchelltford/discordbot/DiscordbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitchelltford.discordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
