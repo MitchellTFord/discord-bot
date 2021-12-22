ba5c22481e4e54e22fff6532c5d4e654d1e2f086
William Ament <will@ament.net>
2021-12-21 23:18:53 -0600

    added discord4j dependency + hello world

--- src/main/java/com/mitchelltford/discordbot/Application.java
+++ src/main/java/com/mitchelltford/discordbot/Application.java
@@ -8,6 +8,21 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+
+    //copied from https://docs.discord4j.com/quickstart/
+    DiscordClient.create("TOKEN")
+        .withGateway(client ->
+            client.on(MessageCreateEvent.class, event -> {
+              Message message = event.getMessage();
+
+              if (message.getContent().equalsIgnoreCase("!ping")) {
+                return message.getChannel()
+                    .flatMap(channel -> channel.createMessage("Pong!"));
+              }
+
+              return Mono.empty();
+            }))
+        .block();
   }
 
 }
