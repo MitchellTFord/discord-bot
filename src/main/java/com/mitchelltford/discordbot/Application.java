36ec65d6502d75156050b711cadcc051046d6a66
LiangZhang21 <69747496+LiangZhang21@users.noreply.github.com>
2021-12-29 16:49:16 -0600

    Update Application.java
    deleted import for ConfigApplicationContext

--- src/main/java/com/mitchelltford/discordbot/Application.java
+++ src/main/java/com/mitchelltford/discordbot/Application.java
@@ -3,7 +3,6 @@ package com.mitchelltford.discordbot;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
