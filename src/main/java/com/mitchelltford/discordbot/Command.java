4db2661879aeb48d9911b64f892b22d601da8ebe
Mitchell Ford <mitchelltford@gmail.com>
2022-01-04 17:49:01 -0600

    Remove unnecessary @Component annotation on Command interface

--- src/main/java/com/mitchelltford/discordbot/Command.java
+++ src/main/java/com/mitchelltford/discordbot/Command.java
@@ -3,10 +3,8 @@ package com.mitchelltford.discordbot;
 import discord4j.core.object.entity.Message;
 import java.util.Collections;
 import java.util.List;
-import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-@Component
 public interface Command {
   String getName();
 
